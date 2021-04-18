966ecbf6ebe2ac7093967c59e8b2687fc40f831d
TheRhenals <therhenalsl@gmail.com>
2021-04-18 00:27:45 -0500

    Added toast in shared service

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
 
-  constructor() { }
+  constructor(
+    private toastController: ToastController
+  ) { }
+
+  async presentToast(data: { message: string, duration?: number, color?: string }) {
+    const toast = await this.toastController.create({
+      message: data.message,
+      duration: data.duration || 3000,
+      color: data.color,
+      buttons: ['OK']
+    });
+    toast.present();
+  }
+
 }
